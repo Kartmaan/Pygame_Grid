f62412f37d0a5a86ef5ab841a799b0530f78d5c3
Kartmaan <cartmanboss69@gmail.com>
2022-03-27 13:04:19 +0200

    Update

--- random_grid.py
+++ random_grid.py
@@ -18,9 +18,15 @@ MARGIN = 5
 # Number of cells on each row
 # Proportional to surface width
 cells_in_row = WIN_WIDTH // (CELL_WIDTH + MARGIN)
+print("-- Dimensions --")
 print("{}x{} px".format(WIN_WIDTH, WIN_HEIGHT))
-print("{}x{} ({}) cells".format(cells_in_row, cells_in_row, cells_in_row**2))
- 
+print("{}x{} ({}) cells\n".format(cells_in_row, cells_in_row, cells_in_row**2))
+
+run = True # Main loop flag
+random_anim = False # Cells color randomization flag
+fps = 60 # Frame Per Second
+anim_speed = 0.05 # Speed animation (second)
+
 # Set colors (RGB)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -57,9 +63,6 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Grille Pygame")
 
 clock = pygame.time.Clock()
-
-run = True
-auto_anim = False
  
 # -------- MAIN LOOP -----------
 while run:
@@ -68,32 +71,37 @@ while run:
             run = False # Exit from main loop
             break
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]: # Clique gauche
-                pos = pygame.mouse.get_pos() # Position du curseur
-                # Conversion des coordonnées x,y en coordonnées grille
-                column = pos[0] // (CELL_WIDTH + MARGIN) # Localisation de la colonne (axe x)
-                row = pos[1] // (CELL_HEIGHT + MARGIN) # Localisation de la ligne (axe y)
+        if event.type == pygame.MOUSEBUTTONDOWN: # Mouse click
+            if pygame.mouse.get_pressed()[0]: # Left click
+                pos = pygame.mouse.get_pos() # Get cursor coordinates
+
+                # Conversion of x,y coordinates to grid coordinates
+                column = pos[0] // (CELL_WIDTH + MARGIN) # Column localisation (x axis)
+                row = pos[1] // (CELL_HEIGHT + MARGIN) # Row localisation (y axis)
+                print("[Column : {} - Row : {}]".format(column, row))
                 """ print("column = {} // ({} + {}) = {}".format(pos[0], CELL_WIDTH, MARGIN, column))
                 print("row = {} // ({} + {}) = {}\n".format(pos[1], CELL_WIDTH, MARGIN, row)) """
                 
-                # Changement d'état de la cellule
+                # Cell state change
+                # These values will define the color with which 
+                # each cell should be drawn
                 if grid[row][column] == 0:
-                    grid[row][column] = 1 # Cellule noire
+                    grid[row][column] = 1 # Black cell
                 else:
-                    grid[row][column] = 0 # Cellule blanche
+                    grid[row][column] = 0 # White cell
             
-            if pygame.mouse.get_pressed()[2]: # Clique droit
-                if auto_anim == False:
-                    auto_anim = True
-                    print("AUTO ANIM TRUE")
+            if pygame.mouse.get_pressed()[2]: # Roght click
+                if random_anim == False:
+                    random_anim = True
+                    print("\nRANDOMIZOR ON\n")
                 else:
-                    auto_anim = False
-                    print("AUTO ANIM FALSE")
+                    random_anim = False
+                    print("\nRANDOMIZOR OFF\n")
  
-    screen.fill("darkblue")
+    # Background color (margin color)
+    screen.fill("darkblue") 
  
-    # Dessin de la grille
+    # GRID DRAWING
     for row in range(cells_in_row):
         for column in range(cells_in_row):
             color = WHITE
@@ -108,7 +116,7 @@ while run:
                             CELL_WIDTH,
                             CELL_HEIGHT])
 
-    if auto_anim:
+    if random_anim:
         x = random.randint(0, cells_in_row-1)
         y = random.randint(0, cells_in_row-1)
 
@@ -117,10 +125,10 @@ while run:
         else:
             grid[x][y] = 1
 
-        time.sleep(0.10)
+        time.sleep(anim_speed)
 
     # Limite de 60 FPS
-    clock.tick(60)
+    clock.tick(fps)
  
     # Mise à jour de l'affichage
     pygame.display.flip()
